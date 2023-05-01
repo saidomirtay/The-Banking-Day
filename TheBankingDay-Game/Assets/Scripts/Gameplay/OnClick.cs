15e467e510907d541d8ef96e2a78e6553474be00
Said Omirtay <88141400+saidomirtay@users.noreply.github.com>
2023-05-01 15:17:00 +0600

    Terrain and bank added

---
+++ TheBankingDay-Game/Assets/Scripts/Gameplay/OnClick.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClick : MonoBehaviour
+{
+    public void OnMouseDown()
+    {
+        Debug.Log("Clicked on model!");
+    }
+}
